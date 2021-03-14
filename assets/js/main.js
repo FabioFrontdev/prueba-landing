38fa272f3fd5e098d61dd4ea4f9893e7c484c4b2
Fabio Leonardo Fuentes <fabioleon.fuentes@gmail.com>
2021-03-14 01:11:33 -0500

    Se aagregan los modulos de sliders y se actualizan los estilos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,152 @@
 
+//---------------------------Script Slider 1-----------------------------------------
+
+//------------------------------ LIST SLIDER -------------------------
+if(document.querySelector('.listslider')){
+   let linkUno = document.querySelectorAll(".listslider li a");
+   linkUno.forEach(function(linkUno) {
+      linkUno.addEventListener('click', function(e){
+         e.preventDefault();
+         let item = this.getAttribute('itlist');
+         let arrItem = item.split("_");
+         fntExecuteSlide(arrItem[1]);
+         return false;
+      });
+    });
+}
+
+function fntExecuteSlide1(side){
+    let parentTarget = document.getElementById('slider');
+    let elements = parentTarget.getElementsByTagName('li');
+    let curElement, nextElement;
+
+    for(var i=0; i<elements.length;i++){
+
+        if(elements[i].style.opacity==1){
+            curElement = i;
+            break;
+        }
+    }
+    if(side == 'prev' || side == 'next'){
+
+        if(side=="prev"){
+            nextElement = (curElement == 0)?elements.length -1:curElement -1;
+        }else{
+            nextElement = (curElement == elements.length -1)?0:curElement +1;
+        }
+    }else{
+        nextElement = side;
+        side = (curElement > nextElement)?'prev':'next';
+
+    }
+    //RESALTA LOS PUNTOS
+    let elementSel = document.getElementsByClassName("listslider")[0].getElementsByTagName("a");
+    elementSel[curElement].classList.remove("item-select-slid");
+    elementSel[nextElement].classList.add("item-select-slid");
+    elements[curElement].style.opacity=0;
+    elements[curElement].style.zIndex =0;
+    elements[nextElement].style.opacity=1;
+    elements[nextElement].style.zIndex =1;
+}
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//------------------------------ Script slider 2 -------------------------
+
+if(document.querySelector('.listslider-2')){
+    let linkDos = document.querySelectorAll(".listslider-2 li a");
+    linkDos.forEach(function(linkDos) {
+       linkDos.addEventListener('click', function(e){
+          e.preventDefault();
+          let item = this.getAttribute('itlist');
+          let arrItem = item.split("_");
+          fntExecuteSlide(arrItem[1]);
+          return false;
+       });
+     });
+ }
+ 
+ function fntExecuteSlide2(side){
+     let parentTarget = document.getElementById('slider-2');
+     let elements = parentTarget.getElementsByTagName('li');
+     let curElement, nextElement;
+ 
+     for(var i=0; i<elements.length;i++){
+ 
+         if(elements[i].style.opacity==1){
+             curElement = i;
+             break;
+         }
+     }
+     if(side == 'prev' || side == 'next'){
+ 
+         if(side=="prev"){
+             nextElement = (curElement == 0)?elements.length -1:curElement -1;
+         }else{
+             nextElement = (curElement == elements.length -1)?0:curElement +1;
+         }
+     }else{
+         nextElement = side;
+         side = (curElement > nextElement)?'prev':'next';
+ 
+     }
+     //RESALTA LOS PUNTOS
+     let elementSel = document.getElementsByClassName("listslider-2")[0].getElementsByTagName("a");
+     elementSel[curElement].classList.remove("item-select-slid");
+     elementSel[nextElement].classList.add("item-select-slid");
+     elements[curElement].style.opacity=0;
+     elements[curElement].style.zIndex =0;
+     elements[nextElement].style.opacity=1;
+     elements[nextElement].style.zIndex =1;
+ }
+
+ //___________________________________________________________________________________________________________________________________________________________
+
+
+//------------------------------ Script slider 3 -------------------------
+if(document.querySelector('.listslider-3')){
+    let linkTres = document.querySelectorAll(".listslider-3 li a");
+    linkTres.forEach(function(linkTres) {
+       linkTres.addEventListener('click', function(e){
+          e.preventDefault();
+          let item = this.getAttribute('itlist');
+          let arrItem = item.split("_");
+          fntExecuteSlide(arrItem[1]);
+          return false;
+       });
+     });
+ }
+ 
+ function fntExecuteSlide3(side){
+     let parentTarget = document.getElementById('slider-3');
+     let elements = parentTarget.getElementsByTagName('li');
+     let curElement, nextElement;
+ 
+     for(var i=0; i<elements.length;i++){
+ 
+         if(elements[i].style.opacity==1){
+             curElement = i;
+             break;
+         }
+     }
+     if(side == 'prev' || side == 'next'){
+ 
+         if(side=="prev"){
+             nextElement = (curElement == 0)?elements.length -1:curElement -1;
+         }else{
+             nextElement = (curElement == elements.length -1)?0:curElement +1;
+         }
+     }else{
+         nextElement = side;
+         side = (curElement > nextElement)?'prev':'next';
+ 
+     }
+     //RESALTA LOS PUNTOS
+     let elementSel = document.getElementsByClassName("listslider-3")[0].getElementsByTagName("a");
+     elementSel[curElement].classList.remove("item-select-slid");
+     elementSel[nextElement].classList.add("item-select-slid");
+     elements[curElement].style.opacity=0;
+     elements[curElement].style.zIndex =0;
+     elements[nextElement].style.opacity=1;
+     elements[nextElement].style.zIndex =1;
+ }
